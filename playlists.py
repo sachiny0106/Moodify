d72bb7776b7111ba0951b0c1a906fe329128c10f
Sachin Yadav <businessboy1508@gmail.com>
2026-01-20 15:07:49 +0530

    feat: add playlist data in `playlists.py`

---
+++ playlists.py
@@ -0,0 +1,170 @@
+PLAYLISTS = {
+    "Soundcloud": {
+        "Hindi": {
+            "Happy": "https://soundcloud.com/miss_happy/sets/hindi-songs",
+            "Sad": "https://soundcloud.com/aryan-ambuj-752291555/sets/sad-hindi-songs-a-little",
+            "Angry": "https://soundcloud.com/user-905375441/sets/hindi-rap-songs",
+            "Fear": "https://soundcloud.com/narendraswapnil/sets/aavirbhaav-a-hindi-horror",
+            "Surprise": "https://soundcloud.com/maryam-zeb-592867892/sets/hot-hindi-hits-2010-2020",
+            "Neutral": "https://soundcloud.com/user635881277/sets/hindi-hits",
+            "Default": "https://soundcloud.com/user635881277/sets/hindi-hits"
+        },
+        "Bengali": {
+            "Happy": "https://soundcloud.com/h-a-i/sets/bengali-songs",
+            "Sad": "https://soundcloud.com/rechna-begum/sets/bengali-songs",
+            "Angry": "https://soundcloud.com/dj-aryan-music",
+            "Fear": "https://soundcloud.com/mushera-khandaker-1/sets/bengali-songs",
+            "Surprise": "https://soundcloud.com/mushera-khandaker-1/sets/bengali-songs",
+            "Neutral": "https://soundcloud.com/ayesha-laskar/sets/bengali-songs",
+            "Default": "https://soundcloud.com/ayesha-laskar/sets/bengali-songs"
+        },
+        "Marathi": {
+            "Happy": "https://soundcloud.com/mahesh-gaikwad-301342141/sets/dj-marathi-song",
+            "Sad": "https://soundcloud.com/ghostblogger/break-up-ke-baad-marathi-song",
+            "Angry": "https://soundcloud.com/sandip-vadde-915668005/sets/marathi-rap",
+            "Fear": "https://soundcloud.com/shrawan-paradkar/sets/marathi-songs",
+            "Surprise": "https://soundcloud.com/mahesh-gaikwad-301342141/sets/dj-marathi-song",
+            "Neutral": "https://soundcloud.com/shrawan-paradkar/sets/marathi-songs",
+            "Default": "https://soundcloud.com/shrawan-paradkar/sets/marathi-songs"
+        },
+        "English": {
+            "Happy": "https://soundcloud.com/gabriela-astudillo-398435247/sets/happy-english-music",
+            "Sad": "https://soundcloud.com/jishnu-rajwani-695997535/sets/famous-english-sad-songs-of",
+            "Angry": "https://soundcloud.com/thomashayden/sets/tech-house-vibes-only",
+            "Fear": "https://soundcloud.com/tito-tito-675324717/sets/horror-english",
+            "Surprise": "https://soundcloud.com/manea-claudia/sets/top-love-songs-2022-playlist-1",
+            "Neutral": "https://soundcloud.com/sejal-agarkar/sets/english-songs-hits",
+            "Default": "https://soundcloud.com/mona-khaled-858700005/sets/english-cringe"
+        },
+        "Punjabi": {
+            "Happy": "https://soundcloud.com/danyal-safir/sets/punjabi-party-songs-2022",
+            "Sad": "https://soundcloud.com/tania-tania-658084779/sets/best-punjabi-sad-songs-2023",
+            "Angry": "https://soundcloud.com/jas-singh-31/sets/gym-punjabi-playlist",
+            "Fear": "https://soundcloud.com/user-94762183/timmy-trumpet-punjabi-x-code_pandorum-murda-fvck-riddim-x-horror-noise-ofdts-mashup",
+            "Surprise": "https://soundcloud.com/itslovesmusic/sets/top-50-punjabi-songs-2022-1",
+            "Neutral": "https://soundcloud.com/tania-tania-658084779/sets/best-punjabi-hits-songs-2023",
+            "Default": "https://soundcloud.com/gvdotbnerxpu/sets/bad-punjabi-remix"
+        },
+        "Telugu": {
+            "Happy": "https://soundcloud.com/sumit-indoria/sets/telugu-party-time",
+            "Sad": "https://soundcloud.com/user-738522704/sets/sad-telugu-songs",
+            "Angry": "https://soundcloud.com/user-692822299/sets/telugu-workout-remix",
+            "Fear": "https://soundcloud.com/vinod-kumar-761560211/sets/telugu-songs-regular-update",
+            "Surprise": "https://soundcloud.com/vinod-kumar-761560211/sets/telugu-songs-regular-update",
+            "Neutral": "https://soundcloud.com/vinod-kumar-761560211/sets/telugu-songs-regular-update",
+            "Default": "https://soundcloud.com/vinod-kumar-761560211/sets/telugu-songs-regular-update"
+        }
+    },
+    "Spotify": {
+        "Hindi": {
+            "Happy": "https://open.spotify.com/embed/playlist/37i9dQZF1DWTwbZHrJRIgD?utm_source=generator&theme=0",
+            "Sad": "https://open.spotify.com/embed/playlist/37i9dQZF1DXdFesNN9TzXT?utm_source=generator&theme=0",
+            "Angry": "https://open.spotify.com/embed/playlist/2r9tRVoHG3AMBTvKJ8abOl?utm_source=generator&theme=0",
+            "Fear": "https://open.spotify.com/embed/playlist/7EZ4lWeM1OLxZYfGmhDbJI?utm_source=generator&theme=0",
+            "Surprise": "https://open.spotify.com/embed/playlist/7vatYrf39uVaZ8G2cVtEik?utm_source=generator&theme=0",
+            "Neutral": "https://open.spotify.com/embed/playlist/37i9dQZF1DX0XUfTFmNBRM?utm_source=generator&theme=0",
+            "Default": "https://open.spotify.com/embed/playlist/37i9dQZF1E4mS880sTV9QE?utm_source=generator&theme=0"
+        },
+        "Bengali": {
+            "Happy": "https://open.spotify.com/embed/playlist/6xW5Vg6ItVIcGzpWpLxrkE?utm_source=generator&theme=0",
+            "Sad": "https://open.spotify.com/embed/playlist/47FrM9FqXeG7l22RZkmoqZ?utm_source=generator&theme=0",
+            "Angry": "https://open.spotify.com/embed/playlist/37i9dQZF1DXcjgclFIY2pu?utm_source=generator&theme=0",
+            "Fear": "https://open.spotify.com/embed/playlist/37i9dQZF1DX0VCG80o22Fb?utm_source=generator&theme=0",
+            "Surprise": "https://open.spotify.com/embed/playlist/0m1idUXUOeJ29oaufW0mLQ?utm_source=generator&theme=0",
+            "Neutral": "https://open.spotify.com/embed/playlist/3RiXySA2fqrWlnAU9n3Lv2?utm_source=generator&theme=0",
+            "Default": "https://open.spotify.com/embed/playlist/7cOYpcvIHxyNdu1DhXkCpu?utm_source=generator&theme=0"
+        },
+        "Marathi": {
+            "Happy": "https://open.spotify.com/embed/playlist/4VyXj7QL7tcyJFM4h1wYe7?utm_source=generator&theme=0",
+            "Sad": "https://open.spotify.com/embed/playlist/37i9dQZF1DWUEPngl1ogec?utm_source=generator&theme=0",
+            "Angry": "https://open.spotify.com/embed/playlist/5dKWOIUR0lmN5URKUVpV6a?utm_source=generator&theme=0",
+            "Fear": "https://open.spotify.com/embed/playlist/37i9dQZF1DX84EApEEEkUc?utm_source=generator&theme=0",
+            "Surprise": "https://open.spotify.com/embed/playlist/37i9dQZF1DX8BKauvV4z7b?utm_source=generator&theme=0",
+            "Neutral": "https://open.spotify.com/embed/playlist/37i9dQZF1DX84EApEEEkUc?utm_source=generator&theme=0",
+            "Default": "https://open.spotify.com/embed/playlist/3sKYXN4FEWLpg4FiJlxSrN?utm_source=generator&theme=0"
+        },
+        "English": {
+            "Happy": "https://open.spotify.com/embed/playlist/37i9dQZF1DXdPec7aLTmlC?utm_source=generator&theme=0",
+            "Sad": "https://open.spotify.com/embed/playlist/37i9dQZF1DX7qK8ma5wgG1?utm_source=generator&theme=0",
+            "Angry": "https://open.spotify.com/embed/playlist/37i9dQZF1EIgNZCaOGb0Mi?utm_source=generator&theme=0",
+            "Fear": "https://open.spotify.com/embed/playlist/37i9dQZF1EIfgYPpPEriFK?utm_source=generator&theme=0",
+            "Surprise": "https://open.spotify.com/embed/playlist/37i9dQZF1EIfIhwClkyzKs?utm_source=generator&theme=0",
+            "Neutral": "https://open.spotify.com/embed/playlist/37i9dQZEVXbMDoHDwVN2tF?utm_source=generator&theme=0",
+            "Default": "https://open.spotify.com/embed/playlist/60eEo5VdhekyGJKTjK2xSV?utm_source=generator&theme=0"
+        },
+        "Punjabi": {
+            "Happy": "https://open.spotify.com/embed/playlist/37i9dQZF1DX7QeQjujWh9l?utm_source=generator&theme=0",
+            "Sad": "https://open.spotify.com/embed/playlist/37i9dQZF1DWWfmbQgZIznF?utm_source=generator&theme=0",
+            "Angry": "https://open.spotify.com/embed/playlist/5ecJF7ZFkufISEw6SZdKrB?utm_source=generator&theme=0",
+            "Fear": "https://open.spotify.com/embed/playlist/37i9dQZF1DWZY9CnWbvq8Q?utm_source=generator&theme=0",
+            "Surprise": "https://open.spotify.com/embed/playlist/37i9dQZF1DWZY9CnWbvq8Q?utm_source=generator&theme=0",
+            "Neutral": "https://open.spotify.com/embed/playlist/37i9dQZF1DWXVJK4aT7pmk?utm_source=generator&theme=0",
+            "Default": "https://open.spotify.com/embed/playlist/3sKYXN4FEWLpg4FiJlxSrN?utm_source=generator&theme=0"
+        },
+        "Telugu": {
+            "Happy": "https://open.spotify.com/embed/playlist/37i9dQZF1DX2UT3NuRgcHd?utm_source=generator&theme=0",
+            "Sad": "https://open.spotify.com/embed/playlist/37i9dQZF1DWUEWjDsV7AgX?utm_source=generator&theme=0",
+            "Angry": "https://open.spotify.com/embed/playlist/37i9dQZF1DX4H5837Y8I1n?utm_source=generator&theme=0",
+            "Fear": "https://open.spotify.com/embed/playlist/4JVDfzNmjX17JnfjaziVQe?utm_source=generator&theme=0",
+            "Surprise": "https://open.spotify.com/embed/playlist/37i9dQZF1DXcuAVsi45c0d?utm_source=generator&theme=0",
+            "Neutral": "https://open.spotify.com/embed/playlist/37i9dQZF1DX6XE7HRLM75P?utm_source=generator&theme=0",
+            "Default": "https://open.spotify.com/embed/playlist/2uTxm6no7uMOHOSYENE7My?utm_source=generator&theme=0"
+        }
+    },
+    "Youtube": {
+        "Hindi": {
+            "Happy": "https://www.youtube.com/watch?v=OcmcptbsvzQ",
+            "Sad": "https://www.youtube.com/watch?v=e6LMUGrN57A",
+            "Angry": "https://www.youtube.com/playlist?list=PLxNm0dqHxmlupV3dr7uq4Rl8L5nwlGKQA",
+            "Fear": "https://www.youtube.com/watch?v=8joIt7wsayU",
+            "Surprise": "https://www.youtube.com/watch?v=rtTI1rh9U5M",
+            "Neutral": "https://www.youtube.com/watch?v=EVF_AuhJgLg",
+            "Default": "https://www.youtube.com/watch?v=S_TW9h7vUB8&list=RDEMyQ80_Idfp-UOKusrpqoh-g&start_radio=1"
+        },
+        "Bengali": {
+            "Happy": "https://www.youtube.com/playlist?list=PLzxTmXYDR-xXsilCSwT-Bds5w96k2GOXF",
+            "Sad": "https://www.youtube.com/watch?v=RRbqeGr8mJE",
+            "Angry": "https://www.youtube.com/watch?v=_RBlE6Ar8mw",
+            "Fear": "https://www.youtube.com/watch?v=5PtVfECix1M",
+            "Surprise": "https://www.youtube.com/watch?v=39uMLYTh40Q",
+            "Neutral": "https://www.youtube.com/watch?v=39uMLYTh40Q",
+            "Default": "https://www.youtube.com/watch?v=5TJqoxsoXc4"
+        },
+        "Marathi": {
+            "Happy": "https://www.youtube.com/playlist?list=PLN_pFG_Bv6D4-gpRS06J2auY87DQaEDuo",
+            "Sad": "https://www.youtube.com/playlist?list=PLpjbqr-x3QIpcfrn_McAvi80s5bGuWr9f",
+            "Angry": "https://www.youtube.com/playlist?list=PLpjbqr-x3QIpw79fcOQJDi3FdMcYwWZki",
+            "Fear": "https://soundcloud.com/miss_happy/sets/hindi-songs",
+            "Surprise": "https://www.youtube.com/watch?v=imUUogeGUio",
+            "Neutral": "https://www.youtube.com/watch?v=xUSTF4Gyj8M",
+            "Default": "https://www.youtube.com/watch?v=xUSTF4Gyj8M"
+        },
+        "English": {
+            "Happy": "https://www.youtube.com/watch?v=ONM4uXLe8CU",
+            "Sad": "https://www.youtube.com/watch?v=BwleOnML1Fo",
+            "Angry": "https://www.youtube.com/watch?v=YnezhNgzdss",
+            "Fear": "https://www.youtube.com/watch?v=G-YNNJIe2Vk",
+            "Surprise": "https://www.youtube.com/watch?v=zStYh2eHOWk",
+            "Neutral": "https://www.youtube.com/watch?v=KNaCJYX-mgY",
+            "Default": "https://www.youtube.com/watch?v=KNaCJYX-mgY"
+        },
+        "Punjabi": {
+            "Happy": "https://www.youtube.com/watch?v=heXb7XQYVKo",
+            "Sad": "https://www.youtube.com/watch?v=8Jq0E9ryVEc",
+            "Angry": "https://www.youtube.com/watch?v=Pha76iGaauM",
+            "Fear": "https://www.youtube.com/watch?v=Id2fc96XPYE",
+            "Surprise": "https://www.youtube.com/watch?v=Id2fc96XPYE",
+            "Neutral": "https://www.youtube.com/watch?v=QbXLf5QvNUM",
+            "Default": "https://www.youtube.com/watch?v=QbXLf5QvNUM"
+        },
+        "Telugu": {
+            "Happy": "https://www.youtube.com/playlist?list=PLMpEfaKcGjpWEgNtdnsvLX6LzQL0UC0EM",
+            "Sad": "https://www.youtube.com/watch?v=yvnsgYQyGXo",
+            "Angry": "https://www.youtube.com/watch?v=CYj6li9939w",
+            "Fear": "https://www.youtube.com/playlist?list=PL4sNEU2Mgm6bNnbM-qKPmTwwDromFqpMQ",
+            "Surprise": "https://www.youtube.com/watch?v=ADqgenM4uqQ",
+            "Neutral": "https://www.youtube.com/playlist?list=PL4sNEU2Mgm6bNnbM-qKPmTwwDromFqpMQ",
+            "Default": "https://www.youtube.com/playlist?list=PL4sNEU2Mgm6bNnbM-qKPmTwwDromFqpMQ"
+        }
+    }
+}
